8340c8b938ae66f8dd8e6b1ac151df6078f5dba8
Егор <tujh1993@mail.ru>
2021-12-24 15:58:11 +0300

    fixtures

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -5,5 +5,5 @@ test('parse', () => {
   expect(parse('file.txt')).toEqual('Wrong file format');
   expect(parse('./notTheRealFile.json')).toEqual('File(s) is not exist');
   expect(parse('./__fixtures__/file1.json')).toEqual(resultForParser);
-  expect(parse('./__fixtures__/file3.yml')).toEqual(resultForParser);
+  expect(parse('./__fixtures__/file1.yml')).toEqual(resultForParser);
 });
