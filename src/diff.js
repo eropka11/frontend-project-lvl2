9d003a3209ee8f24b865b2bc12bb3a1252f68555
Егор <tujh1993@mail.ru>
2022-01-02 10:41:28 +0300

    diff.js mutation fix

--- src/diff.js
+++ src/diff.js
@@ -1,5 +1,12 @@
 import _ from 'lodash';
 
+const makeElement = (elementName, prefix, path, children) => {
+  const element = {
+    name: elementName, prefix, path, children,
+  };
+  return element;
+};
+
 const prefixCorrecter = (children) => {
   if (Array.isArray(children)) {
     const correctedChildren = children.map((child) => {
@@ -15,51 +22,40 @@ const prefixCorrecter = (children) => {
 const diff = (tree1, tree2) => {
   const names1 = tree1.map((element) => element.name);
   const names2 = tree2.map((element) => element.name);
-  const result = tree1.flatMap((currentElement) => {
+  const result1 = tree1.flatMap((currentElement) => {
     if (!names2.includes(currentElement.name)) {
       const correctedCurrentElement = { ...currentElement };
       correctedCurrentElement.children = prefixCorrecter(currentElement.children);
       return correctedCurrentElement;
     }
-    const element = {};
-    element.name = currentElement.name;
-    element.prefix = '  ';
-    element.path = currentElement.path;
     const child1 = currentElement.children;
     const filtred = tree2.filter((element2) => Object.is(element2.name, currentElement.name))[0];
     const child2 = filtred.children;
     if (Array.isArray(child1) && Array.isArray(child2)) {
-      element.children = diff(child1, child2);
-      return element;
+      return makeElement(currentElement.name, '  ', currentElement.path, diff(child1, child2));
     }
     if (child1 === child2) {
-      element.children = child1;
+      return makeElement(currentElement.name, '  ', currentElement.path, child1);
     }
     if (filtred.name === currentElement.name && child1 !== child2) {
-      element.prefix = '- ';
-      element.children = prefixCorrecter(child1);
-      const element2 = {};
-      element2.name = element.name;
-      element2.path = element.path;
-      element2.prefix = '+ ';
-      element2.children = prefixCorrecter(child2);
+      const element = makeElement(currentElement.name, '- ', currentElement.path, prefixCorrecter(child1));
+      const element2 = makeElement(currentElement.name, '+ ', currentElement.path, prefixCorrecter(child2));
       return [element, element2];
     }
-    return element;
+    return currentElement;
   });
-  tree2.map((element) => {
+  const result2 = tree2.flatMap((element) => {
     if (!names1.includes(element.name)) {
       if (Array.isArray(element.children)) {
         const newChild = prefixCorrecter(element.children);
-        const correctedElement = { ...element };
-        correctedElement.children = newChild;
-        result.push(correctedElement);
-      } else {
-        result.push(element);
+        return makeElement(element.name, element.prefix, element.path, newChild);
       }
+      return element;
     }
-    return element;
+    return [];
   });
+
+  const result = result1.concat(result2);
   const sortedResult = _.sortBy(result, 'name');
   return sortedResult;
 };
